20c06805fd8de1c6b8632a484f33fac06a90eabb
Nxyz-natan <natanyemane75@icloud.com>
2026-06-09 23:20:32 -0600

    fixed my level added enemies and now added apples

--- scenes/Player.gd
+++ scenes/Player.gd
@@ -13,57 +13,67 @@ extends CharacterBody2D
 @export var dash_cooldown = 1.0
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var jump: AudioStreamPlayer2D = $jump
+@onready var death_sound: AudioStreamPlayer2D = $Death
 var is_dashing = false 
 var dash_start_position = 0 
 var dash_direction = 0 
 var dash_timer = 0
-
+var alive =true
+var can_move = true
 func _physics_process(delta: float) -> void:
+	if !alive:
+		return
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_force
-		jump.play()
-	if Input.is_action_just_released("jump") and velocity.y < 0:
-		velocity.y *= decelerate_on_jump_release
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = jump_force
+			jump.play()
+		if Input.is_action_just_released("jump") and velocity.y < 0:
+			velocity.y *= decelerate_on_jump_release
 
-	var speed
-	if Input.is_action_pressed("run"):
-		speed= run_speed
-	else:
-		speed = walk_speed
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = move_toward(velocity.x,direction * speed, speed * acelartion)
-		animated_sprite.play("Walk")
-		animated_sprite.flip_h = direction <0
-	else:
-		velocity.x = move_toward(velocity.x, 0, walk_speed * decelartion)
-		animated_sprite.play("idle")
-
-	#dash activation
-	if Input.is_action_just_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
-		is_dashing = true 
-		dash_start_position = position.x
-		dash_direction = direction
-		dash_timer = dash_cooldown
-
-	#preforms actual dash
-	if is_dashing:
-		var current_distance = abs(position.x - dash_start_position)
-		if current_distance >= dash_max_distance or is_on_wall():
-			is_dashing = false
+		var speed
+		if Input.is_action_pressed("run"):
+			speed= run_speed
 		else:
-			velocity.x = dash_direction * dash_speed * dash_curve.sample(current_distance / dash_max_distance)
-			velocity.y = 0
-	
-	#Reduces the dash timer
-	if dash_timer > 0:
-		dash_timer -= delta
+			speed = walk_speed
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = move_toward(velocity.x,direction * speed, speed * acelartion)
+			animated_sprite.play("Walk")
+			animated_sprite.flip_h = direction <0
+		else:
+			velocity.x = move_toward(velocity.x, 0, walk_speed * decelartion)
+			animated_sprite.play("idle")
 
-	move_and_slide()
+		#dash activation
+		if Input.is_action_just_pressed("dash") and direction and not is_dashing and dash_timer <= 0:
+			is_dashing = true 
+			dash_start_position = position.x
+			dash_direction = direction
+			dash_timer = dash_cooldown
+
+		#preforms actual dash
+		if is_dashing:
+			var current_distance = abs(position.x - dash_start_position)
+			if current_distance >= dash_max_distance or is_on_wall():
+				is_dashing = false
+			else:
+				velocity.x = dash_direction * dash_speed * dash_curve.sample(current_distance / dash_max_distance)
+				velocity.y = 0
+		
+		#Reduces the dash timer
+		if dash_timer > 0:
+			dash_timer -= delta
+
+		move_and_slide()
+	
+func die() -> void:
+	death_sound.play()
+	alive = false
+	animated_sprite.play("Hurt")
--- scenes/mainscene.gd
+++ scenes/mainscene.gd
@@ -1,11 +1,43 @@
-extends Node
-
-
+extends Node2D
+var score: int = 1
+var level: int = 1
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	_setup_level()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _setup_level() -> void:
+	var exit = $".".get_node_or_null("Exit")
+	if exit:
+		exit.body_entered.connect(_on_exit_body_entered)
+	#connect apples
+	var apples = $".".get_node_or_null("aplles")
+	if apples:
+		for apple in apples.get_children():
+			apple.collected.connect(increase_score)
+	#connect enemies
+	var enemies = $".".get_node_or_null("enemy")
+	if enemies:
+		for enemy in enemies.get_children():
+			enemy.player_died.connect(_on_player_died)
+			
+			
+
+func _on_exit_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		level += 1
+		body.can_move = false
+
+func _on_player_died(body):
+	body.die()
+	print("Player Killed")
+
+
+func increase_score() -> void:
+	score +=1
+	print(score)
+	
